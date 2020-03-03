3483f127ad102651da72a3dfebaab634f7122ed6
morfiy <edurnev@keaz.ru>
2020-03-03 16:46:57 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -55,4 +55,33 @@ jQuery(function($) {
         }
     });
 });
+function calculateSum (){
+    var sum = 0;
+    var count = 0;
+    $("input[data-price]").each(function(){
+        var c=parseInt($(this).val());
+        count+=c;
+        sum+=parseInt($(this).data("price"))*c;
+    });
 
+    $(".js-count-total").text(count);
+    $(".js-sum-total").text(sum);
+    $(".js-sum-total-value").val(sum);
+}
+$('.minus').on("click",function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 0 ? 0 : count;
+    $input.val(count);
+    calculateSum();
+    $(this).parent().parent().siblings('.card-title-sum').text($input.val()*parseInt($input.data('price')));
+    return false;
+});
+$('.plus').click(function (event) {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    calculateSum();
+    $(this).parent().parent().siblings('.card-title-sum').text($input.val()*parseInt($input.data('price')));
+
+    return false;
+});
